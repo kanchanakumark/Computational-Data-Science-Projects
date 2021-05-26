aff2c18548de379692fc3556253f0e3c629f4317
Kanchana Kumar <77928197+kanchanakumark@users.noreply.github.com>
2021-05-26 23:04:05 +0530

    Create Linear Regression with Regularization.py
    Draft version

---
+++ Linear Regression with Regularization.py
@@ -0,0 +1,13 @@
+# Loading the Required Packages
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import r2_score
+
