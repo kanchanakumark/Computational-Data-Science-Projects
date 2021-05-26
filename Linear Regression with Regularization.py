10cfa021d9fb4a4c9d54b88c1e91f3875c068337
Kanchana Kumar <77928197+kanchanakumark@users.noreply.github.com>
2021-05-26 23:12:29 +0530

    Update Linear Regression with Regularization.py
    Added few visualizations

--- Linear Regression with Regularization.py
+++ Linear Regression with Regularization.py
@@ -11,3 +11,29 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import r2_score
 
+# Read the hour.csv file
+df_hour = pd.read_csv("/content/hour.csv")
+df_hour.head(5)
+df_hour.dtypes
+
+
+fig = plt.figure(figsize=(10,5))
+plt.bar(df_hour['hr'],df_hour['cnt'])
+plt.xlabel("Hour")
+plt.ylabel("Number of bikes rented")
+plt.title("Bikes Rental Distribution");
+
+fig = plt.figure(figsize=(15,6))
+sns.histplot(data=df_hour, x='cnt');
+plt.xlabel('Bikes Rented')
+plt.ylabel('Frequency');
+
+fig = plt.figure(figsize=(15,6))
+sns.histplot(data=df_hour, x='casual');
+plt.xlabel('casual')
+plt.ylabel('Frequency');
+
+fig = plt.figure(figsize=(15,6))
+sns.histplot(data=df_hour, x='registered');
+plt.xlabel('Bikes Registered')
+plt.ylabel('Frequency');
